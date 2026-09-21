0835972f2732d18d183cf8a5e6ce6330c87be01c
Kyro-Qu <qushichao75@gmail.com>
2026-09-21 23:47:14 +0800

    chore: bump asset versions and add no-cache meta tags to bust browser cache

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ import { Scope } from "./ui/scope.js";
 import { Dashboard } from "./ui/dashboard.js";
 import { Terminal } from "./ui/terminal.js";
 import { ControlConsole, PRESET_COMMANDS, MODES, IDENT_COMMANDS, FEEDBACK_COMMANDS, MODE_CONTROLS } from "./ui/console.js";
-import { WorkflowWizard } from "./ui/wizard.js";
+import { WorkflowWizard } from "./ui/wizard.js?v=0.5.54";
 import { MathChannels, MATH_OPS } from "./ui/math.js";
 import { TriggerEngine, TriggerMode } from "./ui/trigger.js";
 import { ScopeLegend } from "./ui/legend.js";
